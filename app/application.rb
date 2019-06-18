e2a14d42fa7ea57418438b07af34a6563897ab93
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 20:03:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/songs"
+    if req.path=="/items"
       resp.write "You requested the songs"
     else
       resp.write "Route not found"
