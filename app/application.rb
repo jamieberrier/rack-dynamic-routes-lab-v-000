f2b29866c4640cf5d8acde5d66670db97b0721b3
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 20:16:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find { |i| i.name == item_name }
       if item.nil?
-        resp.write ""
+        resp.write "Item not found"
         resp.status = 400
       resp.write item.price
     else
